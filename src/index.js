b919f3595037886c5400f36aa4bbcf89c36a063f
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-05-23 11:21:34 -0700

    removed /index from routing and changed it to /

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ import Lavender from "views/examples/Lavender.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route path="/" render={(props) => <Index {...props} />} />
       <Route
         path="/nucleo-icons"
         render={(props) => <NucleoIcons {...props} />}
