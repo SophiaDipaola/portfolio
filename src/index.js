d329cd96caf5a1ad7b7b47b6aab62fdc566650c2
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-05-28 11:16:54 -0700

    index

--- src/index.js
+++ src/index.js
@@ -38,32 +38,32 @@ import Lavender from "views/examples/Lavender.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route path="/" render={(props) => <Index {...props} />} />
       <Route
         path="/nucleo-icons"
         render={(props) => <NucleoIcons {...props} />}
       />
       <Route
-        path="/Selene"
+        path="/selene"
         render={(props) => <Selene {...props} />}
       />
       <Route
-        path="/About"
+        path="/about"
         render={(props) => <ProfilePage {...props} />}
       />
       <Route
-        path="/Farmony"
+        path="/farmony"
         render={(props) => <Farmony {...props} />}
       />
       <Route
-        path="/MindWholeness"
+        path="/mindWholeness"
         render={(props) => <Mind {...props} />}
       />
       <Route
-        path="/Lavender"
+        path="/lavender"
         render={(props) => <Lavender {...props} />}
       />
-      <Redirect to="/index" />
+      <Redirect to="/" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
